142a4fa46a16ab8e5f3f1fd6ed88747a6499e619
Ankor <ankor_socialki@mail.ru>
2023-03-21 16:40:52 +0300

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Andrei Kovryzhenko on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
