2bd8972dee6179cede70a2a4b9f8930413d94a0c
Ankor <ankor_socialki@mail.ru>
2023-03-31 19:52:39 +0300

    v1.0

---
+++ OnlineShop/PageOne/LatestProduct.swift
@@ -0,0 +1,20 @@
+//
+//  LatestProduct.swift
+//  OnlineShop
+//
+//  Created by Andrei Kovryzhenko on 29.03.2023.
+//
+
+import SwiftUI
+
+struct LatestProduct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LatestProduct_Previews: PreviewProvider {
+    static var previews: some View {
+        LatestProduct()
+    }
+}
---
+++ OnlineShop/WelcomeBackView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeBackView.swift
+//  OnlineShop
+//
+//  Created by Andrei Kovryzhenko on 23.03.2023.
+//
+
+import SwiftUI
+
+struct WelcomeBackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeBackView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeBackView()
+    }
+}
